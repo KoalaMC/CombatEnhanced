87cb79fef4631b8a9cf47de371e72094dc13dc53
Ricky Chon <rickychon99@gmail.com>
2021-03-26 05:13:45 -0400

    Init project

---
+++ src/main/java/io/github/rickyc0626/CombatEnhanced.java
@@ -0,0 +1,20 @@
+package io.github.rickyc0626;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CombatEnhanced extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}
